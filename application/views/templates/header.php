76179e556cdb4b08258985fa87a2dfdd35a7a35d
XI <khchow@xandi.co.jp>
2021-03-24 19:46:07 +0600

    add bootstrap4 and home, about link into nav of header

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -1,6 +1,19 @@
 <html>
 	<head>
-		<title>CodeIgniter Tutorial</title>
+		<title>CodeIgniter</title>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 	</head>
 	<body>
-		<h1><?= $title ?></h1>
+		<nav class="navbar navbar-inverse">
+			<div class="container">
+				<div class="navbar-header">
+					<a class="navbar-brand" href="<?= base_url() ?>">CodeIgniter</a>
+				</div>
+				<div id="navbar">
+					<ul class="nav navbar-nav">
+						<li><a href="<?= base_url() ?>">Home</a></li>
+						<li><a href="<?= base_url() ?>/about">About</a></li>
+					</ul>
+				</div>
+			</div>
+		</nav>
