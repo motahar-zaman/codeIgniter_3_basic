b2b98de310d294d289df48e2ed9e742962013e82
XI <khchow@xandi.co.jp>
2021-03-24 20:04:57 +0600

    header design change

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -4,15 +4,30 @@
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 	</head>
 	<body>
-		<nav class="navbar navbar-inverse">
-			<div class="container">
-				<div class="navbar-header">
-					<a class="navbar-brand" href="<?= base_url() ?>">CodeIgniter</a>
+		<style>
+			.bg-primary {
+				background-color:#007aeb;
+			}
+			.bg-gray {
+				background-color:#cccccc;
+			}
+			.txt-white{
+				color:#fff;
+			}
+		</style>
+		<nav class="bg-primary">
+			<div class="row container">
+				<div class="col-md-6">
+					<a class="txt-white" href="<?= base_url() ?>"><h4>CodeIgniter</h4></a>
 				</div>
-				<div id="navbar">
-					<ul class="nav navbar-nav">
-						<li><a href="<?= base_url() ?>">Home</a></li>
-						<li><a href="<?= base_url() ?>/about">About</a></li>
+				<div class="col-md-6">
+					<ul class="row">
+						<li class="col">
+							<a class="txt-white" href="<?= base_url() ?>">Home</a>
+						</li>
+						<li class="col">
+							<a class="txt-white" href="<?= base_url() ?>/about">About</a>
+						</li>
 					</ul>
 				</div>
 			</div>
